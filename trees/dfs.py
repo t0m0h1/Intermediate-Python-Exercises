f56ed97482c53f366db259e3392e7df1e80e552c
t0m0h1 <104913403+t0m0h1@users.noreply.github.com>
2024-06-05 23:59:55 +0100

    Refactor depth_first_search function in dfs.py

--- trees/dfs.py
+++ trees/dfs.py
@@ -2,22 +2,22 @@ from binarytree import Node
 from binarytree import BinaryTree
 
 # Description: Depth First Search implementation for a binary tree.
-def depth_first_search(self, start, target):
+def depth_first_search(start, target):
     if start is None:
         return False
     if start.data == target:
         return True
-    left = self.depth_first_search(start.left, target)
-    right = self.depth_first_search(start.right, target)
+    left = depth_first_search(start.left, target)
+    right = depth_first_search(start.right, target)
     return left or right
 
-tree = BinaryTree(1)
-tree.root.left = Node(2)
-tree.root.right = Node(3)
-tree.root.left.left = Node(4)
-tree.root.left.right = Node(5)
+tree1 = BinaryTree(1)
+tree1.root.left = Node(2)
+tree1.root.right = Node(3)
+tree1.root.left.left = Node(4)
+tree1.root.left.right = Node(5)
 
 if __name__ == '__main__':
-    print(depth_first_search(tree, tree.root, 5)) # True
-    print(depth_first_search(tree, tree.root, 6)) # False
-    print(depth_first_search(tree, tree.root, 3)) # True
+    print(depth_first_search(tree1.root, 5)) # True
+    print(depth_first_search(tree1.root, 6)) # False
+    print(depth_first_search(tree1.root, 3)) # True
