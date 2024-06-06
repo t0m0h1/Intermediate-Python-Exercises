f3aa1251985405e0ccd6ec2575b5430a1e2af325
t0m0h1 <104913403+t0m0h1@users.noreply.github.com>
2024-06-06 17:42:01 +0100

    Refactor gcd_and_lcm.py to improve efficiency

--- gcd_and_lcm.py
+++ gcd_and_lcm.py
@@ -1,18 +1,12 @@
 def greatest_common_divisor(n1, n2):
-    divisors = []
-    for i in range(1, n1):
-        for j in range(1, n2):
-            common_d = i % j == 0
-            divisors.append(common_d)
-
-    return max(divisors)
-
-
-
+    while n2 != 0:
+        n1, n2 = n2, n1 % n2
+    return n1
 
 def lowest_common_multiple(n1, n2):
-    pass
+    return n1 * n2 // greatest_common_divisor(n1, n2)
 
 
 if __name__ == '__main__':
     print(greatest_common_divisor(12, 18)) # 6
+    print(lowest_common_multiple(12, 18)) # 36
