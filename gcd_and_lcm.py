e9712a0a605dd20e86e5d043d8b9924e0d9756ca
t0m0h1 <104913403+t0m0h1@users.noreply.github.com>
2024-06-06 17:33:26 +0100

    Implement greatest_common_divisor function and add test case

--- gcd_and_lcm.py
+++ gcd_and_lcm.py
@@ -5,9 +5,14 @@ def greatest_common_divisor(n1, n2):
             common_d = i % j == 0
             divisors.append(common_d)
 
+    return max(divisors)
 
 
 
 
 def lowest_common_multiple(n1, n2):
     pass
+
+
+if __name__ == '__main__':
+    print(greatest_common_divisor(12, 18)) # 6
