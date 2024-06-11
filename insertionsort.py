a980f22001478f0f9d709b032e9ce0c00d618813
t0m0h1 <104913403+t0m0h1@users.noreply.github.com>
2024-06-11 15:19:07 +0100

    Add random number generation to insertion sort algorithm

--- insertionsort.py
+++ insertionsort.py
@@ -1,4 +1,5 @@
 from typing import List
+import random
 
 def insertion_sort(A: List[int]) -> List:
     n = len(A)
@@ -14,11 +15,13 @@ def insertion_sort(A: List[int]) -> List:
 
     return A
     
+numbers = [random.randint(0, 1000) for i in range(1000)]
+
 # driver code
 if __name__ == '__main__':
     print(insertion_sort([5, 2, 4, 6, 1, 3])) # [1, 2, 3, 4, 5, 6]  
     print(insertion_sort([31, 41, 59, 26, 41, 58])) # [26, 31, 41, 41, 58, 59]
-
+    print(insertion_sort(numbers))
 
 # Time complexity: O(n^2) which is the worst case scenario
 # Space complexity: O(1)
